f2f58be704d6c6312b8a7322a1aab1c471f75e8a
Peter Brown <peterbrownpa@gmail.com>
2015-12-29 13:28:31 -0800

    set up waitList controller show how data flows to view model

---
+++ src/app/waitList/waitList.controller.js
@@ -0,0 +1,17 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('app.waitList')
+    .controller('WaitListController', WaitListController);
+
+  function WaitListController(){
+    // vm is 'this' instance of view model object
+    // https://github.com/johnpapa/angular-styleguide#style-y032
+    var vm = this;
+
+    vm.parties = ["data flowing"];
+
+  }
+
+})();
