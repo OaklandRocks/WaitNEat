0878182e52d64686206967a1f31389eda7795a97
Peter Brown <peterbrownpa@gmail.com>
2016-01-04 12:33:31 -0800

    can't get firebase injector services to work

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -4,18 +4,20 @@
   angular
     .module('app.waitList')
     .controller('WaitListController', WaitListController);
+  WaitListController.$inject = ['firebaseArray'];
 
-  function WaitListController(){
+  function WaitListController($firebaseArray){
     // vm is 'this' instance of controller instance
     // https://github.com/johnpapa/angular-styleguide#style-y032
     var vm = this;
 
-    vm.parties = ["data flowing"];
+    var fireParties = new Firebase('https://waitandeat-demo-ve.firebaseio.com/');
+    vm.parties = $firebaseArray(fireParties);
 
     vm.addParty = addParty;
 
     function addParty(){
-      vm.parties.push('whats up diggy dig dog');
+      vm.parties.$add('another');
     }
 
   }
