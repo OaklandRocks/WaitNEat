e90f846f3f500606e51edd739516650785313690
Peter Brown <peterbrownpa@gmail.com>
2016-01-07 20:09:25 -0800

    register user function with firebaseReference and AuthObject

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -7,6 +7,21 @@
 
   AuthController.$inject =['$firebaseAuth']
   function AuthController($firebaseAuth){
+    var vm = this;
+    // firebase reference
+    var firebaseReference = new Firebase('https://waitandeat-demo-ve.firebaseio.com');
+    var firebaseAuthObject = $firebaseAuth(firebaseReference);
+
+    vm.user ={
+      email: '',
+      password: ''
+    };
+    vm.register = register;
+
+
+    function register(user){
+      return firebaseAuthObject.$createUser(user);
+    }
 
   }
 
