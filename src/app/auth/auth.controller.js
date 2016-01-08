1d6f28b7dfa5300bcc77845e67fa7f8445bdddda
Peter Brown <peterbrownpa@gmail.com>
2016-01-07 21:06:59 -0800

    cant get auth register to work XMLHttpRequest errors

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -6,10 +6,11 @@
     .controller('AuthController', AuthController);
 
   AuthController.$inject =['$firebaseAuth']
+
   function AuthController($firebaseAuth){
     var vm = this;
     // firebase reference
-    var firebaseReference = new Firebase('https://waitandeat-demo-ve.firebaseio.com');
+    var firebaseReference = new Firebase('https://waitandeat-demo-ve.firebaseio.com/');
     var firebaseAuthObject = $firebaseAuth(firebaseReference);
 
     vm.user ={
