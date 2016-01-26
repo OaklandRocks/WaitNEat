ed621fdc92b57caef002bcaa49efcab10d274b7e
Peter Brown <peterbrownpa@gmail.com>
2016-01-25 17:07:23 -0800

    create TextMessage service

---
+++ src/app/core/textMessage.service.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.core')
+    .factory('textMessageService', textMessageService)
+  textMessageService.$inject = ['firebaseDataService']
+  function textMessageService(firebaseDataService) {
+    var service = {
+      sendTextMessage: sendTextMessage
+    };
+
+    return service;
+
+    //////////////////////
+
+    function sendTextMessage(butts, parties){
+      var newTextMessage = {
+        phoneNumber: butts.phone,
+        size: butts.size,
+        name: butts.name
+      }
+      firebaseDataService.textMessages.push(newTextMessage);
+      butts.notified = true;
+      vm.parties.$save(butts);
+    }
+
+  }
+
+})();
